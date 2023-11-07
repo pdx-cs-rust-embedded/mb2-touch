4b9fee93ffa0ffb6dbe68dc2e492fa1c4ebb4464
Bart Massey <bart.massey@gmail.com>
2023-11-07 10:46:35 -0800

    added library and example

---
+++ examples/read_loop.rs
@@ -0,0 +1,29 @@
+#![no_main]
+#![no_std]
+
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+use cortex_m_rt::entry;
+use microbit::{
+    board::Board,
+    hal::{prelude::*, timer},
+};
+
+use mb2_touch::*;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let touch_pin = board.pins.p1_04.into_floating_input();
+    let timer0 = timer::Timer::new(board.TIMER0);
+    let mut timer1 = timer::Timer::new(board.TIMER1);
+    let mut touchpad = Touchpad::new(touch_pin, timer0);
+
+    loop {
+        let count = touchpad.sense();
+        rprintln!("{}", count);
+        timer1.delay_ms(500u16);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#![no_main]
+#![no_std]
+
+use panic_rtt_target as _;
+use microbit::hal::{prelude::*, pac, gpio, timer};
+
+pub struct Touchpad {
+    pin: Option<gpio::p1::P1_04<gpio::Input<gpio::Floating>>>,
+    timer: timer::Timer<pac::TIMER0>,
+}
+
+impl Touchpad {
+    pub fn new(
+        pin: gpio::p1::P1_04<gpio::Input<gpio::Floating>>,
+        timer: timer::Timer<pac::TIMER0>,
+    ) -> Self {
+        Touchpad { pin: Some(pin), timer }
+    }
+
+    pub fn sense(&mut self) -> u32 {
+        let pin = self.pin.take().unwrap();
+        let touch_pin = pin.into_push_pull_output(gpio::Level::Low);
+        self.timer.delay_ms(10u16);
+        let touch_pin = touch_pin.into_floating_input();
+        let mut count = 0u32;
+        while touch_pin.is_low().unwrap() {
+            self.timer.delay_us(1u16);
+            count += 1;
+        }
+        self.pin = Some(touch_pin);
+        count
+    }
+}
