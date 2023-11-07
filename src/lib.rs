b3885174b05dab36b044a81fa542aa08e51e0a2a
Bart Massey <bart.massey@gmail.com>
2023-11-07 13:17:31 -0800

    finished initial typestate split; have not fixed example

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,62 @@
 #![no_main]
 #![no_std]
 
-use panic_rtt_target as _;
+use core::marker::PhantomData;
+
 use microbit::hal::{prelude::*, pac, gpio, timer};
 
-pub enum TouchpadState {
-    Idle,
-    Setup,
-    Sense,
-}
-
-pub struct TouchpadIdle;
-pub struct TouchpadSetup;
-pub struct TouchpadSense;
+pub enum TouchpadIdle {}
+pub enum TouchpadSetup {}
+pub enum TouchpadSense {}
 
 pub trait TouchpadState {}
 
 impl TouchpadState for TouchpadIdle {}
+impl TouchpadState for TouchpadSetup {}
+impl TouchpadState for TouchpadSense {}
 
 pub struct Touchpad<T: TouchpadState> {
-    pin: Option<gpio::p1::P1_04<gpio::Input<gpio::Floating>>>,
+    pin: gpio::p1::P1_04<gpio::Input<gpio::Floating>>,
     timer: timer::Timer<pac::TIMER0>,
+    p: PhantomData<T>,
 }
 
-impl Touchpad {
+impl Touchpad<TouchpadIdle> {
     pub fn new(
         pin: gpio::p1::P1_04<gpio::Input<gpio::Floating>>,
         timer: timer::Timer<pac::TIMER0>,
     ) -> Self {
-        Touchpad { pin: Some(pin), timer }
+        Touchpad { pin, timer, p: PhantomData }
     }
+}
 
-    pub fn sense(&mut self) -> u32 {
-        let pin = self.pin.take().unwrap();
-        let touch_pin = pin.into_push_pull_output(gpio::Level::Low);
+
+impl Touchpad<TouchpadIdle> {
+    pub fn setup(mut self) -> Touchpad<TouchpadSense> {
+        let touch_pin = self.pin.into_push_pull_output(gpio::Level::Low);
         self.timer.delay_ms(10u16);
         let touch_pin = touch_pin.into_floating_input();
+        let touchpad = Touchpad {
+            pin: touch_pin,
+            timer: self.timer,
+            p: PhantomData,
+        };
+        touchpad
+    }
+}
+
+impl Touchpad<TouchpadSense> {
+    pub fn sense(mut self) -> (u32, Touchpad<TouchpadIdle>) {
         let mut count = 0u32;
-        while touch_pin.is_low().unwrap() {
+        while self.pin.is_low().unwrap() {
             self.timer.delay_us(1u16);
             count += 1;
         }
-        self.pin = Some(touch_pin);
-        count
+        let touchpad = Touchpad {
+            pin: self.pin,
+            timer: self.timer,
+            p: PhantomData,
+        };
+        (count, touchpad)
     }
 }
