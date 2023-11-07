b6a6ff80f7b9d1408431f2c0482bcd7af0422975
Henk Oordt <hd@oordt.dev>
2023-11-07 22:14:13 +0100

    Make touchpad work with interrupts

---
+++ examples/read_event.rs
@@ -0,0 +1,72 @@
+#![no_main]
+#![no_std]
+
+use core::{cell::RefCell, sync::atomic::AtomicU32};
+
+use cortex_m::interrupt::Mutex;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+use cortex_m_rt::entry;
+use microbit::{
+    board::Board,
+    hal::{gpiote, pac, timer},
+};
+use pac::interrupt;
+
+use mb2_touch::*;
+
+static TOUCHPAD: Mutex<RefCell<Option<Touchpad>>> = Mutex::new(RefCell::new(None));
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let touch_pin = board.pins.p1_04.into_floating_input();
+    let timer0 = timer::Timer::new(board.TIMER0);
+    let gpiote = gpiote::Gpiote::new(board.GPIOTE);
+
+    let touchpad = Touchpad::new(touch_pin, timer0, gpiote, interrupt::SWI0_EGU0, 100);
+    cortex_m::interrupt::free(|cs| {
+        TOUCHPAD.borrow(cs).borrow_mut().replace(touchpad);
+    });
+
+    // Safety: we're outside any critical sections, and are thus not messing with one
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(interrupt::SWI0_EGU0);
+        cortex_m::peripheral::NVIC::unmask(interrupt::GPIOTE);
+        cortex_m::peripheral::NVIC::unmask(interrupt::TIMER0);
+    }
+
+    loop {
+        // rprintln!("Loop");
+        cortex_m::asm::wfe();
+    }
+}
+
+#[interrupt]
+fn GPIOTE() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(t) = TOUCHPAD.borrow(cs).borrow_mut().as_mut() {
+            t.handle_gpio_interrupt();
+        }
+    })
+}
+
+#[interrupt]
+fn TIMER0() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(t) = TOUCHPAD.borrow(cs).borrow_mut().as_mut() {
+            t.handle_timer_interrupt();
+        }
+    })
+}
+
+static TOUCH_COUNT: AtomicU32 = AtomicU32::new(0);
+#[interrupt]
+fn SWI0_EGU0() {
+    rprintln!(
+        "Ouch! {}",
+        TOUCH_COUNT.fetch_add(1, core::sync::atomic::Ordering::Relaxed)
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,117 @@
 #![no_main]
 #![no_std]
 
+use core::fmt;
+
+use microbit::hal::{
+    gpio::{self, p1::P1_04, Floating, Input, Output, Pin, PushPull},
+    gpiote::Gpiote,
+    pac,
+    prelude::*,
+    timer,
+};
 use panic_rtt_target as _;
-use microbit::hal::{prelude::*, pac, gpio, timer};
+
+pub enum TouchpadState {
+    Disabled(Pin<Input<Floating>>),
+    Idle(Pin<Input<Floating>>),
+    Setup(Pin<Output<PushPull>>),
+    Sense(Pin<Input<Floating>>, u32),
+}
+
+impl fmt::Debug for TouchpadState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Disabled(_) => f.debug_tuple("Disabled").finish(),
+            Self::Idle(_) => f.debug_tuple("Idle").finish(),
+            Self::Setup(_) => f.debug_tuple("Setup").finish(),
+            Self::Sense(_, arg1) => f.debug_tuple("Sense").field(arg1).finish(),
+        }
+    }
+}
 
 pub struct Touchpad {
-    pin: Option<gpio::p1::P1_04<gpio::Input<gpio::Floating>>>,
     timer: timer::Timer<pac::TIMER0>,
+    state: Option<TouchpadState>,
+    gpiote: Gpiote,
+    interrupt: pac::Interrupt,
+    threshold: u32,
 }
 
 impl Touchpad {
     pub fn new(
-        pin: gpio::p1::P1_04<gpio::Input<gpio::Floating>>,
+        pin: P1_04<Input<Floating>>,
         timer: timer::Timer<pac::TIMER0>,
+        gpiote: Gpiote,
+        interrupt: pac::Interrupt,
+        threshold: u32,
     ) -> Self {
-        Touchpad { pin: Some(pin), timer }
+        let pin = pin.degrade();
+        gpiote
+            .channel0()
+            .input_pin(&pin)
+            .hi_to_lo()
+            .disable_interrupt();
+
+        let mut t = Touchpad {
+            state: Some(TouchpadState::Idle(pin)),
+            timer,
+            gpiote,
+            interrupt,
+            threshold,
+        };
+        t.handle_timer_interrupt();
+        t
     }
 
-    pub fn sense(&mut self) -> u32 {
-        let pin = self.pin.take().unwrap();
-        let touch_pin = pin.into_push_pull_output(gpio::Level::Low);
-        self.timer.delay_ms(10u16);
-        let touch_pin = touch_pin.into_floating_input();
-        let mut count = 0u32;
-        while touch_pin.is_low().unwrap() {
-            self.timer.delay_us(1u16);
-            count += 1;
-        }
-        self.pin = Some(touch_pin);
-        count
+    pub fn handle_timer_interrupt(&mut self) {
+        self.timer.event_compare_cc0().reset();
+        let new_state = match self.state.take().unwrap() {
+            TouchpadState::Idle(pin) => {
+                let pin = pin.into_push_pull_output(gpio::Level::Low);
+                self.timer.enable_interrupt();
+                self.timer.start(10000u32); // TODO correct value
+                TouchpadState::Setup(pin)
+            }
+            TouchpadState::Setup(pin) => {
+                self.timer.start(1u32);
+                self.timer.enable_interrupt();
+                let pin = pin.into_floating_input();
+                self.gpiote
+                    .channel0()
+                    .input_pin(&pin)
+                    .hi_to_lo()
+                    .enable_interrupt();
+                TouchpadState::Sense(pin, 0)
+            }
+            TouchpadState::Sense(pin, count) => {
+                self.timer.start(1u32);
+                self.timer.enable_interrupt();
+                TouchpadState::Sense(pin, count + 1)
+            }
+            s => s,
+        };
+
+        self.state.replace(new_state);
+    }
+
+    pub fn handle_gpio_interrupt(&mut self) {
+        self.gpiote.channel0().reset_events();
+        let new_state = match self.state.take().unwrap() {
+            TouchpadState::Sense(pin, count) => {
+                self.gpiote
+                    .channel0()
+                    .input_pin(&pin)
+                    .hi_to_lo()
+                    .disable_interrupt();
+                if count > self.threshold {
+                    cortex_m::peripheral::NVIC::pend(self.interrupt);
+                }
+                TouchpadState::Idle(pin)
+            }
+            s => s,
+        };
+
+        self.state.replace(new_state);
     }
 }
