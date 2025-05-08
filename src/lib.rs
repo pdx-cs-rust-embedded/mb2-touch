6428ce94bdc494246d86359eb4ed97be07c83e30
Bart Massey <bart.massey@gmail.com>
2025-05-08 15:14:55 -0700

    moved to a shorter reset time

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use panic_rtt_target as _;
 use embedded_hal::{delay::DelayNs, digital::InputPin};
 use microbit::hal::gpio;
 
+pub const RESET_TIME: u32 = 10;
+
 pub type TouchPin = gpio::Pin<gpio::Input<gpio::Floating>>;
 
 pub struct Touchpad<T> {
@@ -23,7 +25,7 @@ impl<T: DelayNs> Touchpad<T> {
     pub fn sense(&mut self, max_ticks: u32) -> u32 {
         let pin = self.pin.take().unwrap();
         let touch_pin = pin.into_push_pull_output(gpio::Level::Low);
-        self.timer.delay_ms(10);
+        self.timer.delay_us(RESET_TIME);
         let mut touch_pin = touch_pin.into_floating_input();
         let mut count = 0u32;
         while count < max_ticks && touch_pin.is_low().unwrap() {
